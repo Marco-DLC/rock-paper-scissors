9da0bc19aa8892f4900a61e800d2848998446aa1
Marco DLC <140775824+Marco-DLC@users.noreply.github.com>
2024-02-05 20:33:24 -0500

    Create getComputerChoice function
    This function randomly returns either rock, paper, or scissors

---
+++ script.js
@@ -0,0 +1,16 @@
+function getComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * 3);
+
+    switch (randomNumber) {
+        case 0:
+            return 'Rock';
+        case 1:
+            return 'Paper';
+        case 2:
+            return 'Scissors';
+    }
+}
+
+const computerChoice = getComputerChoice();
+
+console.log(computerChoice);
